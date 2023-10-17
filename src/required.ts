45242ace7696cbfc92de6dc996daf681c040e266
stagas <gstagas@gmail.com>
2023-10-17 15:19:26 +0300

    save

--- src/required.ts
+++ src/required.ts
@@ -2,46 +2,47 @@ import { NonNull } from './types'
 
 let requiredTarget: any
 
+export const MissingDependencyErrorSymbol = Symbol('MissingDependencyError')
+
 export class MissingDependencyError extends Error {
   constructor(prop: string) {
     super(`Property "${prop}" is required to be a non-nullish value.`)
     this.name = 'MissingDependencyError'
-    // @ts-ignore
     Error.captureStackTrace(this, requiredProxyHandler.get)
   }
 }
 
-const requiredProxyHandler = {
-  get(_: any, prop: any) {
-    console.log(prop in requiredTarget, requiredTarget[prop], requiredTarget[prop], requiredTarget[prop])
-    if (prop in requiredTarget && requiredTarget[prop] != null) {
-      return requiredTarget[prop]
-    }
-    throw new MissingDependencyError(prop)
-  }
-}
-
-export const MissingDependencyErrorSymbol = Symbol('MissingDependencyError')
-
 const requiredProxyHandlerFast = {
   get(_: any, prop: any) {
-    console.log(prop, requiredTarget[prop])
-    if (prop in requiredTarget && requiredTarget[prop] != null) {
-      return requiredTarget[prop]
+    // requiredTarget can be overwritten at the getter, so we use a const copy here.
+    const t = requiredTarget
+    if (prop in t && t[prop] != null) {
+      return t[prop]
     }
     throw MissingDependencyErrorSymbol
   }
 }
 
-const RequiredProxy = new Proxy({}, requiredProxyHandler)
-const RequiredProxyFast = new Proxy({}, requiredProxyHandlerFast)
-
-export function required<T extends object>(of: T): NonNull<T> {
-  requiredTarget = of
-  return RequiredProxy as any
+const requiredProxyHandler = {
+  get(_: any, prop: any) {
+    // requiredTarget can be overwritten at the getter, so we use a const copy here.
+    const t = requiredTarget
+    if (prop in t && t[prop] != null) {
+      return t[prop]
+    }
+    throw new MissingDependencyError(prop)
+  }
 }
+
+const RequiredProxyFast = new Proxy({}, requiredProxyHandlerFast)
+const RequiredProxy = new Proxy({}, requiredProxyHandler)
 
 export function requiredFast<T extends object>(of: T): NonNull<T> {
   requiredTarget = of
   return RequiredProxyFast as any
+}
+
+export function required<T extends object>(of: T): NonNull<T> {
+  requiredTarget = of
+  return RequiredProxy as any
 }
