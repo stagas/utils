d9408f96b3403dca167501d0d8cb477c1fa42e9a
stagas <gstagas@gmail.com>
2023-12-02 13:29:24 +0200

    save

--- src/checksum.ts
+++ src/checksum.ts
@@ -1,9 +1,9 @@
 // credits: chat-gpt
 export function checksum(str: string) {
-  let hash = 0;
+  let hash = 0
   for (let i = 0; i < str.length; i++) {
-    hash = (hash << 6) - hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32-bit integer
+    hash = (hash << 6) - hash + str.charCodeAt(i)
+    hash = hash & hash // Convert to 32-bit integer
   }
   return Math.abs(hash)
 }
