04cd10382028c420f2a0d051c1998ee9d017bd11
stagas <gstagas@gmail.com>
2023-12-14 11:37:45 +0200

    save

--- src/hurry.ts
+++ src/hurry.ts
@@ -3,3 +3,9 @@ import { timeout } from './timeout.ts'
 export function hurry<T>(ms: number, promise: Promise<T>): Promise<T | void> {
   return Promise.race([promise, timeout(ms)])
 }
+
+export function hurryThrow<T>(ms: number, promise: Promise<T>): Promise<T | void> {
+  return Promise.race([promise, timeout(ms).then(() =>
+    Promise.reject(new Error('Timed out: >' + ms + 'ms'))
+  )])
+}
