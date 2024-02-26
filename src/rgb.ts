40e4268a9afc4485df401355ac56a73df2f90bcf
stagas <gstagas@gmail.com>
2024-02-26 16:10:37 +0200

    save

--- src/rgb.ts
+++ src/rgb.ts
@@ -16,91 +16,107 @@ type HSL = [number, number, number]
 export function hslToRgb([h, s, l]: HSL): RGB {
   h /= 360
 
-  let r, g, b;
+  let r, g, b
 
   if (s == 0) {
-    r = g = b = l; // achromatic
+    r = g = b = l // achromatic
   } else {
     const hue2rgb = function hue2rgb(p: number, q: number, t: number) {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
+      if (t < 0) t += 1
+      if (t > 1) t -= 1
+      if (t < 1 / 6) return p + (q - p) * 6 * t
+      if (t < 1 / 2) return q
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+      return p
     }
 
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+    const p = 2 * l - q
+    r = hue2rgb(p, q, h + 1 / 3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1 / 3)
   }
 
-  return [r, g, b];
+  return [r, g, b]
 }
 
 // Source: https://stackoverflow.com/a/23502424 .
 export function rgbToHsl([r, g, b]: RGB): HSL {
   // r /= 255, g /= 255, b /= 255;
-  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  const max = Math.max(r, g, b), min = Math.min(r, g, b)
   // eslint-disable-next-line prefer-const
-  let h = 0, s, l = (max + min) / 2;
+  let h = 0, s, l = (max + min) / 2
 
   if (max == min) {
-    h = s = 0; // achromatic
+    h = s = 0 // achromatic
   } else {
-    const d = max - min;
-    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    const d = max - min
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
     switch (max) {
-      case r: h = (g - b) / d; break;
-      case g: h = 2 + ((b - r) / d); break;
-      case b: h = 4 + ((r - g) / d); break;
+      case r: h = (g - b) / d; break
+      case g: h = 2 + ((b - r) / d); break
+      case b: h = 4 + ((r - g) / d); break
     }
-    h *= 60;
-    if (h < 0) h += 360;
+    h *= 60
+    if (h < 0) h += 360
   }
-  return [h, s, l];
+  return [h, s, l]
+}
+
+export function hueshift(c: string, a: number): string {
+  // Convert the color from hex to RGB
+  const rgb = hexToRgb(c)
+
+  // Convert the RGB values to HSL
+  const hsl = rgbToHsl(rgb)
+
+  hsl[0] = (hsl[0] + a) % 360
+
+  // Convert the HSL values back to RGB
+  const newRgb = hslToRgb(hsl)
+
+  // Convert the RGB values back to hex and return the result
+  return rgbToHex(newRgb)
 }
 
 export function luminate(c: string, a: number): string {
   // Convert the color from hex to RGB
-  const rgb = hexToRgb(c);
+  const rgb = hexToRgb(c)
 
   // Convert the RGB values to HSL
-  const hsl = rgbToHsl(rgb);
+  const hsl = rgbToHsl(rgb)
 
   // Increase the lightness value by the specified amount
-  hsl[2] += a;
+  hsl[2] += a
 
   // Clamp the lightness value between 0 and 1
-  hsl[2] = Math.max(0, Math.min(1, hsl[2]));
+  hsl[2] = Math.max(0, Math.min(1, hsl[2]))
 
   // Convert the HSL values back to RGB
-  const newRgb = hslToRgb(hsl);
+  const newRgb = hslToRgb(hsl)
 
   // Convert the RGB values back to hex and return the result
-  return rgbToHex(newRgb);
+  return rgbToHex(newRgb)
 }
 
 export function saturate(c: string, a: number): string {
   // Convert the color from hex to RGB
-  const rgb = hexToRgb(c);
+  const rgb = hexToRgb(c)
 
   // Convert the RGB values to HSL
-  const hsl = rgbToHsl(rgb);
+  const hsl = rgbToHsl(rgb)
 
   // Increase the saturation value by the specified amount
-  hsl[1] += a;
+  hsl[1] += a
 
   // Clamp the saturation value between 0 and 1
   // hsl[1] = Math.max(0, Math.min(1, hsl[1]));
 
   // Convert the HSL values back to RGB
-  const newRgb = hslToRgb(hsl);
+  const newRgb = hslToRgb(hsl)
 
   // Convert the RGB values back to hex and return the result
-  return rgbToHex(newRgb);
+  return rgbToHex(newRgb)
 }
 
 export function hexToRgb(hex: string): RGB {
@@ -110,20 +126,20 @@ export function hexToRgb(hex: string): RGB {
     hex = hex
       .split('')
       .map((x) => `${x}${x}`)
-      .join('');
+      .join('')
   }
 
   // Convert the hex string to an array of numbers
   const hexValues = hex
     .match(/[\da-f]{2}/gi)!.map((x) =>
       Math.max(0, Math.min(1, parseInt(x, 16) / 255))
-    );
+    )
 
   // Extract the red, green, and blue values from the hex array
-  const [r, g, b] = hexValues.slice(0, 3);
+  const [r, g, b] = hexValues.slice(0, 3)
 
   // Return the RGB values as an array
-  return [r, g, b];
+  return [r, g, b]
 }
 
 export function rgbToHex(rgb: RGB): string {
@@ -132,8 +148,8 @@ export function rgbToHex(rgb: RGB): string {
     Math.max(0, Math.min(255, Math.round(x * 255)))
       .toString(16)
       .padStart(2, '0')
-  );
+  )
 
   // Concatenate the hex strings into a single string and return the result
-  return `#${hexValues.join('')}`;
+  return `#${hexValues.join('')}`
 }
